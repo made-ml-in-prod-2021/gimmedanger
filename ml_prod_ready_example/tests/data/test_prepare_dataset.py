792e64d58c3e5676a97a174d12705de0fd32e7f3
GimmeDanger <vladim1r.nazarov@yandex.ru>
2021-05-10 13:40:45 +0300

    NEW: hw1: data + features: tests

---
+++ ml_prod_ready_example/tests/data/test_prepare_dataset.py
@@ -0,0 +1,49 @@
+import unittest
+
+from typing import NoReturn
+
+from entities.split_train_val_params import SplitTrainValParams
+
+from data.prepare_dataset \
+    import prepare_data, read_data, split_train_val_data, \
+    FILENAME, ORIG_SHAPE, ORIG_COLUMNS, THALASSEMIA_TYPES, \
+    SEX_TYPES, CHEST_PAIN_TYPES, FASTING_BLOOD_SUGAR_TYPES, \
+    REST_ECG_TYPES, EXERCISE_INDUCED_ANGINA_TYPES, ST_SLOPE_TYPES, \
+    FINAL_COLUMNS, FINAL_CAT_COLUMNS, FINAL_NUM_COLUMNS
+
+
+class PrepareDatasetTestCase(unittest.TestCase):
+    def test_read_data(self) -> NoReturn:
+        df = read_data(f'data/{FILENAME}')
+        self.assertEqual(df.shape, ORIG_SHAPE)
+        self.assertTupleEqual(tuple(df.columns), ORIG_COLUMNS)
+
+    def test_prepare_data(self) -> NoReturn:
+        df = read_data(f'data/{FILENAME}')
+        mod_df, cat_features, num_features = prepare_data(df)
+        self.assertEqual(mod_df.shape, ORIG_SHAPE)
+        self.assertTupleEqual(tuple(mod_df.columns), FINAL_COLUMNS)
+        self.assertTupleEqual(cat_features, FINAL_CAT_COLUMNS)
+        self.assertTupleEqual(num_features, FINAL_NUM_COLUMNS)
+        self.assertSetEqual(set(mod_df['thalassemia'].unique()), set(THALASSEMIA_TYPES))
+        self.assertSetEqual(set(mod_df['sex'].unique()), set(SEX_TYPES))
+        self.assertSetEqual(set(mod_df['chest_pain_type'].unique()), set(CHEST_PAIN_TYPES))
+        self.assertSetEqual(set(mod_df['fasting_blood_sugar'].unique()), set(FASTING_BLOOD_SUGAR_TYPES))
+        self.assertSetEqual(set(mod_df['rest_ecg'].unique()), set(REST_ECG_TYPES))
+        self.assertSetEqual(set(mod_df['exercise_induced_angina'].unique()), set(EXERCISE_INDUCED_ANGINA_TYPES))
+        self.assertSetEqual(set(mod_df['st_slope'].unique()), set(ST_SLOPE_TYPES))
+
+    def test_split_train_val_data(self) -> NoReturn:
+        df = read_data(f'data/{FILENAME}')
+        # regular case
+        params = SplitTrainValParams(test_size=0.1)
+        train_df, val_df = split_train_val_data(df, params)
+        self.assertEqual(len(val_df) / len(train_df), 0.11397058823529412)
+        # grid search case
+        params = SplitTrainValParams(test_size=0.0)
+        train_df, val_df = split_train_val_data(df, params)
+        self.assertEqual(len(val_df) / len(train_df), params.test_size)
+
+
+if __name__ == '__main__':
+    unittest.main()
