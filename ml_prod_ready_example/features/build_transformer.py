bbbbabc2466f3a17533aae3336d2a3dc49b7fd23
GimmeDanger <vladim1r.nazarov@yandex.ru>
2021-05-10 13:39:50 +0300

    NEW: hw1: features prepare: src

---
+++ ml_prod_ready_example/features/build_transformer.py
@@ -0,0 +1,38 @@
+from sklearn.preprocessing import OneHotEncoder, MinMaxScaler
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+
+from entities.feature_params import FeatureParams
+
+
+def build_categorical_pipeline() -> Pipeline:
+    return Pipeline(
+        [
+            ('OneHot', OneHotEncoder()),
+        ]
+    )
+
+
+def build_numerical_pipeline() -> Pipeline:
+    return Pipeline(
+        [
+            ('Normalize', MinMaxScaler()),
+        ]
+    )
+
+
+def build_transformer(params: FeatureParams) -> ColumnTransformer:
+    return ColumnTransformer(
+        [
+            (
+                "categorical_pipeline",
+                build_categorical_pipeline(),
+                params.categorical_features,
+            ),
+            (
+                "numerical_pipeline",
+                build_numerical_pipeline(),
+                params.numerical_features,
+            ),
+        ]
+    )
