a4d12260339dc638f14864c4464732837a2e1986
GimmeDanger <vladim1r.nazarov@yandex.ru>
2021-06-14 23:02:20 +0300

    NEW: hw3: part 3a: add sensors

--- airflow_ml_dags/dags/train_pipeline.py
+++ airflow_ml_dags/dags/train_pipeline.py
@@ -1,6 +1,6 @@
 from airflow import DAG
 from airflow.providers.docker.operators.docker import DockerOperator
-from airflow.operators.bash import BashOperator
+from airflow.sensors.filesystem import FileSensor
 from airflow.utils.dates import days_ago
 
 
@@ -16,6 +16,10 @@ with DAG(
 
     PREPARE_INPUT_PATH = 'data/raw/{{ ds }}/data.csv'
     PREPARE_OUTPUT_PATH = 'data/processed/{{ ds }}/data.csv'
+    prepare_input_data_sensor = FileSensor(
+        task_id='prepare_input_data_sensor',
+        filepath=PREPARE_INPUT_PATH
+    )
     prepare_data = DockerOperator(
         command=f'{PREPARE_INPUT_PATH} {PREPARE_OUTPUT_PATH}',
         image='airflow-pipeline-prepare-data',
@@ -28,6 +32,14 @@ with DAG(
     SPLIT_CONFIG_PATH = 'configs/train_val_split.yaml'
     SPLIT_INPUT_PATH = 'data/processed/{{ ds }}/data.csv'
     SPLIT_OUTPUT_PATH = 'data/processed/{{ ds }}'
+    split_config_sensor = FileSensor(
+        task_id='split_config_sensor',
+        filepath=SPLIT_CONFIG_PATH
+    )
+    split_input_data_sensor = FileSensor(
+        task_id='split_input_data_sensor',
+        filepath=SPLIT_INPUT_PATH
+    )
     train_val_split = DockerOperator(
         command=f'{SPLIT_CONFIG_PATH} {SPLIT_INPUT_PATH} {SPLIT_OUTPUT_PATH}',
         image='airflow-pipeline-train-val-split',
@@ -40,6 +52,14 @@ with DAG(
     TRAIN_CONFIG_PATH = 'configs/train_model.yaml'
     TRAIN_INPUT_PATH = 'data/processed/{{ ds }}/train_data.csv'
     TRAIN_OUTPUT_PATH = 'data/models/{{ ds }}.pkl'
+    train_config_sensor = FileSensor(
+        task_id='train_config_sensor',
+        filepath=TRAIN_CONFIG_PATH
+    )
+    train_input_data_sensor = FileSensor(
+        task_id='train_input_data_sensor',
+        filepath=TRAIN_INPUT_PATH
+    )
     train_model = DockerOperator(
         command=f'{TRAIN_CONFIG_PATH} {TRAIN_INPUT_PATH} {TRAIN_OUTPUT_PATH}',
         image='airflow-pipeline-train-model',
@@ -52,6 +72,14 @@ with DAG(
     EVAL_MODEL_PATH = 'data/models/{{ ds }}.pkl'
     EVAL_INPUT_PATH = 'data/processed/{{ ds }}/val_data.csv'
     EVAL_OUTPUT_PATH = 'data/evals/{{ ds }}.txt'
+    eval_model_sensor = FileSensor(
+        task_id='eval_model_sensor',
+        filepath=EVAL_MODEL_PATH
+    )
+    eval_input_data_sensor = FileSensor(
+        task_id='eval_input_data_sensor',
+        filepath=EVAL_INPUT_PATH
+    )
     eval_model = DockerOperator(
         command=f'{TRAIN_CONFIG_PATH} {EVAL_MODEL_PATH} {EVAL_INPUT_PATH} {EVAL_OUTPUT_PATH}',
         image='airflow-pipeline-eval-model',
@@ -61,4 +89,7 @@ with DAG(
         volumes=[f'{PROJECT_PATH}/configs:/configs', f'{PROJECT_PATH}/data:/data']
     )
 
-    prepare_data >> train_val_split >> train_model >> eval_model
+    prepare_input_data_sensor >> prepare_data >> \
+    split_config_sensor >> split_input_data_sensor >> train_val_split >> \
+    train_config_sensor >> train_input_data_sensor >> train_model >> \
+    eval_model_sensor >> eval_input_data_sensor >> eval_model
