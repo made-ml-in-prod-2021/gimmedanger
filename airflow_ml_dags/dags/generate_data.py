114539b91f0ed4fe766b13a8d2b8fb90c5e34d2c
GimmeDanger <vladim1r.nazarov@yandex.ru>
2021-06-19 16:08:49 +0300

    FIX: hw3: move PATH_TO_PROJECT and DEFAULT_ARGS to single place

--- airflow_ml_dags/dags/generate_data.py
+++ airflow_ml_dags/dags/generate_data.py
@@ -1,30 +1,21 @@
-from datetime import timedelta
-
 from airflow import DAG
 from airflow.providers.docker.operators.docker import DockerOperator
 from airflow.sensors.filesystem import FileSensor
 from airflow.utils.dates import days_ago
 
+from constants import PROJECT_PATH, DEFAULT_ARGS
 
-LOCAL_CONFIG_PATH = 'configs/generate_data.yaml'
-LOCAL_OUTPUT_PATH = 'data/raw/{{ ds }}/data.csv'
-PROJECT_PATH = '/opt/hhd-drive/Yandex.Disk/Computer Science/courses/ml/made-ml-in-prod/hws/airflow_ml_dags'
-
-
-default_args = {
-    "owner": "airflow",
-    "email": ["vladim1r.nazarov@yandex.ru"],
-    "retries": 1,
-    "retry_delay": timedelta(minutes=5),
-}
 
 with DAG(
     dag_id='generate-data',
-    default_args=default_args,
+    default_args=DEFAULT_ARGS,
     start_date=days_ago(1),
     schedule_interval='@hourly',
     max_active_runs=1,
 ) as dag:
+
+    LOCAL_CONFIG_PATH = 'configs/generate_data.yaml'
+    LOCAL_OUTPUT_PATH = 'data/raw/{{ ds }}/data.csv'
     config_sensor = FileSensor(
         task_id='config_sensor',
         filepath=LOCAL_CONFIG_PATH
--- airflow_ml_dags/dags/predict.py
+++ airflow_ml_dags/dags/predict.py
@@ -1,24 +1,14 @@
-from datetime import timedelta
-
 from airflow import DAG
 from airflow.providers.docker.operators.docker import DockerOperator
 from airflow.sensors.filesystem import FileSensor
 from airflow.utils.dates import days_ago
 
-
-PROJECT_PATH = '/opt/hhd-drive/Yandex.Disk/Computer Science/courses/ml/made-ml-in-prod/hws/airflow_ml_dags'
-
-default_args = {
-    "owner": "airflow",
-    "email": ["vladim1r.nazarov@yandex.ru"],
-    "retries": 1,
-    "retry_delay": timedelta(minutes=5),
-}
+from constants import PROJECT_PATH, DEFAULT_ARGS
 
 
 with DAG(
     dag_id='predict',
-    default_args=default_args,
+    default_args=DEFAULT_ARGS,
     start_date=days_ago(1),
     schedule_interval='@daily',
     max_active_runs=1,
